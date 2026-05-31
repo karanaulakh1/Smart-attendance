9a42906b22b53ade964699ca1b50bd807755ac4f
karanaulakh1 <karanaulakh1717@gmail.com>
2026-05-31 12:22:59 +0530

    j

--- database.php
+++ database.php
@@ -1,16 +1,16 @@
 <?php
 
-$conn = new mysqli(
+$conn = mysqli_init();
+
+$conn->real_connect(
     "gateway01.ap-southeast-1.prod.alicloud.tidbcloud.com",
     "JmzF5kyqRvEwnxn.root",
     "nRavqZA6tYW3LGNf",
     "attendance_system",
-    4000
+    4000,
+    NULL,
+    MYSQLI_CLIENT_SSL
 );
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
 
 echo "Connected!";
 ?>
