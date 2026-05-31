9f2de0d45cea39d6fd835c0b867c3ab79e9fdbc0
karanaulakh1 <karanaulakh1717@gmail.com>
2026-05-31 12:50:26 +0530

    o

--- database.php
+++ database.php
@@ -1,16 +1,24 @@
 <?php
 
+$host = "gateway01.ap-southeast-1.prod.alicloud.tidbcloud.com";
+$user = "JmzF5kyqRvEwnxn.root";
+$pass = "nRavqZA6tYW3LGNf";
+$db   = "attendance_system";
+$port = 4000;
+
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
 $conn = mysqli_init();
 
+mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
+
 $conn->real_connect(
-    "gateway01.ap-southeast-1.prod.alicloud.tidbcloud.com",
-    "JmzF5kyqRvEwnxn.root",
-    "nRavqZA6tYW3LGNf",
-    "attendance_system",
-    4000,
+    $host,
+    $user,
+    $pass,
+    $db,
+    $port,
     NULL,
     MYSQLI_CLIENT_SSL
 );
-
-echo "Connected!";
 ?>
