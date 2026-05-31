50d82d563698e73f685ee5209b19f8d709ba884c
karanaulakh1 <karanaulakh1717@gmail.com>
2026-05-31 12:14:45 +0530

    p

--- database.php
+++ database.php
@@ -1,20 +1,16 @@
 <?php
 
+$conn = new mysqli(
+    "gateway01.ap-southeast-1.prod.alicloud.tidbcloud.com",
+    "JmzF5kyqRvEwnxn.root",
+    "nRavqZA6tYW3LGNf",
+    "attendance_system",
+    4000
+);
 
-$host = getenv("gateway01.ap-southeast-1.prod.alicloud.tidbcloud.com");     
-$user = getenv("JmzF5kyqRvEwnxn.root");          
-$password = getenv("nRavqZA6tYW3LGNf");         
-$dbname = getenv("attendance_system");  
-$port = getenv("4000");
-// Create connection
-$conn = new mysqli($host, $user, $password, $dbname, $port);
-
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Optional success message (for testing)
-// echo "Connected successfully";
-
+echo "Connected!";
 ?>
